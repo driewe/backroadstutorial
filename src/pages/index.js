b78fe50407d7031296dcd40713640d2d692f5575
David Riewe <david.riewe@gmail.com>
2020-04-07 17:15:10 -0500

    update 97 CSS fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
 import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 
-export default () => 
+export default () => (
         <Layout>
-            <div>hello from backroads</div>
-
+            <SimpleHero>
+                <Banner title="continue exploring" info="Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.">
+                <Link to="/tours" className="btn-white">explore tours</Link>
+                </Banner>
+            </SimpleHero>
         </Layout>
+)
