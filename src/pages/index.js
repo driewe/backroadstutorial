5e39fea99df852dda1ec21f238ad93bd5276a521
David Riewe <david.riewe@gmail.com>
2020-04-11 16:33:48 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import {Link} from 'gatsby'
 import Layout from '../components/Layout'
 import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
+import About from '../components/Home/About'
+import Services from "../components/Home/Services"
+
 
 export default () => (
         <Layout>
@@ -11,5 +14,7 @@ export default () => (
                 <Link to="/tours" className="btn-white">explore tours</Link>
                 </Banner>
             </SimpleHero>
+            <About />
+            <Services />
         </Layout>
 )
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
 
+
 export default class tours extends Component {
     render() {
-        return <Layout>Hello from Tours page!!!!</Layout>
+        return <Layout>Hello from Tours page!!!!
+            
+        </Layout>
     }
 }
